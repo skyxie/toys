07f274dec09126bcb8a2845cf9c131e5ce3c4516
Tian Xie <tian@animoto.com>
2015-01-14 12:41:00 -0500

    Added tool to convert /proc/<pid>/maps address range to human-readable size

---
+++ address_range_size/address_range_size.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+HELP = <<_HELP_
+#{$0} <maps file>
+
+Determines the size of memory allocated for every line noted in /proc/<pid>/maps
+_HELP_
+
+def exit_with_help msg
+  puts msg if msg
+  puts HELP
+  exit(1)
+end
+
+# Reduces size measured in bytes to a human-readable size
+def human_size size_bytes
+  size = size_bytes
+  type = "B"
+
+  %w{KB MB GB}.each do |t|
+    if size > 1024
+      size = size/1024
+      type = t
+    else
+      break
+    end
+  end
+
+  "#{size}#{type}"
+end
+
+def parse_maps_line line
+  maps_line = line.gsub(/\s+/, ' ').split(' ')
+  lib = maps_line[5]
+  address_range = maps_line[0]
+
+  addresses = address_range.split("-")
+  if addresses.length != 2
+    exit_with_help("Invalid address format, expects address1-address2")
+  end
+
+  int1, int2 = *(addresses.map { |a| a.to_i(16) })
+  size = int2 - int1
+  
+  puts "#{human_size(size)} #{lib}"
+end
+
+if !ARGV[0]
+  exit_with_help("Invalid number of arguments")
+end
+
+if !File.exists?(ARGV[0])
+  exit_with_help("maps file not found!")
+end
+
+maps_file = ARGV[0]
+File.readlines(maps_file).each do |line|
+  parse_maps_line line.chomp
+end
